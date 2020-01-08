3b16b31c7c7e3e2b27ccbcb99f1f38aab69fc2ea
arcum-omni <hut8dev@gmail.com>
2020-01-08 14:24:48 -0800

    Created Student DB Script

---
+++ BranchingExample/DBScripts/CreateStudentDB.sql
@@ -0,0 +1,13 @@
+﻿create database Student
+go
+
+use Student
+go
+
+create table Student
+(
+	FirstName		varchar(25)		not null
+	,LastName		varchar(25)		not null
+	,DateOfBirth	date			null
+	,CellPhone		char(10)		null
+)
